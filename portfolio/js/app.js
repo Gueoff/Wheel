6064de3121d7433e2034c2de5215bd649a1c599a
DESBROSSES <geoffrey.desbrosses@gmail.com>
2017-03-26 20:37:51 +0200

    portfolio with notes

--- portfolio/js/app.js
+++ portfolio/js/app.js
@@ -5,12 +5,26 @@
 */
 
 
-angular.module('app', [])
+var app = angular.module('app', []);
 
-	.controller('ProjectController',['$scope', '$http', function($scope, $http){
+	app.controller('ProfileController', ['$scope', function($scope){
+		$scope.user = {
+			name: 'DESBROSSES',
+			firstname: 'Geoffrey',
+			birthdate : 12/09/1994,
+			description: 'Hey guys ! That\'s me, I\'m a french full stack developer. I\'m living in Nantes(44) since 2015, where I have made some stuff, I invite you to take a look of my website.'
+		};
+	}]);
+
+	app.controller('ProjectController',['$scope', '$http', function($scope, $http){
+		$scope.description = 'Here we are ! That\'s a list of all my personnal project you can access.';
+		
 		$scope.projects = [
-		{name:'Wheel', description:'Drink mobile game', date:01/01/17, plateform: 'mobile', versions:[{name:'1.0.0', download:'http://xd.com'}, {name:'2.0.0', download:'http://lol.com'}]}
+		{id:1, name:'Wheel', description:'Drink mobile game', date:'01/01/2017', plateform: 'mobile', downloads:[{name:'1.0.0', download:'http://xd.com'}, {name:'2.0.0', download:'http://lol.com'}]},
+		{id:2, name:'Margaux Noée', description:'Web site for a photograph', date:'01/01/2017', plateform:'web', website:'http://margauxnoee.com'}
 		];
-		
-		
-	}])
+	}]);
+	
+	app.controller('ContactController', ['$scope', function($scope){
+		$scope.description = 'Now you have seen my works, you can send me a message or contact me via linkedin.';
+	}]);
